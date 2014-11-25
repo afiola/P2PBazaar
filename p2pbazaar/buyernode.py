eb4037c1aacea9a3b9d2ae6cb60ef21d71c7afaa
Andrew Fiola <andrew.fiola@gmail.com>
2014-11-25 14:35:55 -0800

    Add skeletons for BuyerNode and corresponding tests

---
+++ p2pbazaar/buyernode.py
@@ -0,0 +1,17 @@
+from p2pbazaar.p2pnode import P2PNode
+import threading
+import json
+from p2pbazaar import trackerPort
+
+class BuyerNode(P2PNode):
+    def __init__(self):
+        pass
+        
+    def searchItem(targetItem):
+        pass
+        
+    def handleReceivedNode(self, inPacketData, inExpectingPing = False, inExpectingTIM = False):
+        pass
+        
+    def handleSearchReply(self, searchReply):
+        pass
---
+++ p2pbazaar/test/buyernodetest.py
@@ -0,0 +1,29 @@
+import socket
+import threading
+import unittest
+import json
+from p2pbazaar.buyernode import BuyerNode
+from p2pbazaar import trackerPort
+
+class BuyerNodeTest(unittest.TestCase):
+    def setUp(self):
+        pass
+    
+    def tearDown(self):
+        pass
+        
+class SearchItemTestCase(BuyerNodeTest):
+    def runTest(self):
+        pass
+        
+class BuyItemTestCase(BuyerNodeTest):
+    def runTest(self):
+        pass
+    
+class HandleReceivedNodeTestCase(BuyerNodeTest):
+    def runTest(self):
+        pass
+        
+class HandleSearchReplyTestCase(BuyerNodeTest):
+    def runTest(self):
+        pass
